4123998bf0fd7a270ec6e3391773c69e91fbe3a3
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2023-12-23 19:12:21 +0400

    added script

--- app/src/database/scripts.sql
+++ app/src/database/scripts.sql
@@ -1,7 +1,10 @@
+select * from deal
+order by id;
+
 select strategy, avg(profit_percentage) as avg_profit_percentage
 from deal
 where 
 	exit_price is null 
 	and exit_date is null
 	and strategy = 'dual_momentum'
-group by strategy
+group by strategy;
