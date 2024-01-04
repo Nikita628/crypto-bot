51878a2332f0113a7fd0011fa8c0a0ddbdafb9b4
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-05 00:31:01 +0300

    env config

--- www/server.py
+++ www/server.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from subprocess import call
 from datetime import datetime
 import requests
@@ -7,7 +9,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 hostName = "193.233.133.115"
 serverPort = 8000
 
-class webHookServer(BaseHTTPRequestHandler):
+class DeploymentServer(BaseHTTPRequestHandler):
     def do_POST(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
@@ -16,18 +18,19 @@ class webHookServer(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         data_unicode = post_data.decode('utf-8')
         data = json.loads(data_unicode)
-        if data.get('ref') == 'refs/heads/prod':
+        if data.get('ref') == 'refs/heads/migrations_and_python_server':
             try:
                 call("./scripts/files_update", shell=True)
 
-                TOKEN = '6874810972:AAG6QTkvcU21ift9Ddpfe3IQNEgj5DxAqOw'
-                CHAT_ID = '-1002072371752'
-                SEND_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+                load_dotenv()
+                TG_STATUS_GROUP_CHAT_ID = os.getenv('TG_STATUS_GROUP_CHAT_ID')
+                TG_BOT_TOKEN = os.getenv('TG_BOT_TOKEN')
+                SEND_URL = f'https://api.telegram.org/bot{TG_BOT_TOKEN}/sendMessage'
                 message = f'<b>Crypto-bot message</b>\n'
                 message += f'<b>Action:</b> update files\n'
                 message += f'<b>Result:</b> success\n'
                 message += f'<b>DateTime:</b> ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                response = requests.post(SEND_URL, json={'chat_id': CHAT_ID, 'parse_mode': 'html', 'text': message})
+                response = requests.post(SEND_URL, json={'chat_id': TG_STATUS_GROUP_CHAT_ID, 'parse_mode': 'html', 'text': message})
                 if response:
                     print('result is sent to telegram')
                 else:
@@ -38,7 +41,7 @@ class webHookServer(BaseHTTPRequestHandler):
 
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = HTTPServer((hostName, serverPort), DeploymentServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
