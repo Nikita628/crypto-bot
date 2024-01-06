ece64e63b9442cf596f17784d476654b24a4699f
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-07 02:16:28 +0300

    add flask server

--- www/deployment_server/app.py
+++ www/deployment_server/app.py
@@ -4,25 +4,26 @@ from dotenv import load_dotenv
 from subprocess import call
 from datetime import datetime
 import requests
+import json
 
 app = Flask(__name__)
 
+load_dotenv()
+CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
+CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
+
 @app.route('/', methods=['GET', 'POST'])
 def do_post():
-    load_dotenv()
-    CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
-    CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
     if request.method == 'POST':
-        if request.args.get('ref') == 'refs/heads/migrations_and_python_server':
-            with open('./output.txt', 'a') as f:
-                f.write('Hi')
-            return 'r'
+        post_data = json.loads(request.data)
+        if post_data.get('ref') == 'refs/heads/migrations_and_python_server':
+            try:
+                call("/var/www/scripts/deploying 2> /var/www/logs/deploying_err.log", shell=True)
+            except:
+                return 'Update error. Calling bash script is crashed'
+            return 'prod branch action'
         else:
-            with open('./output.txt', 'a') as f:
-                f.write('TTTTT')
-            return CRYPTO_BOT_STATUS_CHAT_ID + CRYPTO_BOT_TOKEN
-    else:
-        return CRYPTO_BOT_STATUS_CHAT_ID + CRYPTO_BOT_TOKEN
+            return 'other branch action'
 
 if __name__ == '__main__':
     app.run(debug=True)
