32e6d4f6ad63c4a6b1366823bfa405fa4c8d3c8f
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-07 01:48:29 +0300

    add flask server

--- www/deployment_server/app.py
+++ www/deployment_server/app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request
+import os
+from dotenv import load_dotenv
+from subprocess import call
+from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def do_post():
+    load_dotenv()
+    CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
+    CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
     if request.method == 'POST':
         if request.args.get('ref') == 'refs/heads/migrations_and_python_server':
             with open('./output.txt', 'a') as f:
@@ -12,9 +20,9 @@ def do_post():
         else:
             with open('./output.txt', 'a') as f:
                 f.write('TTTTT')
-            return 'post'
+            return CRYPTO_BOT_STATUS_CHAT_ID + CRYPTO_BOT_TOKEN
     else:
-        return 'not post request'
+        return CRYPTO_BOT_STATUS_CHAT_ID + CRYPTO_BOT_TOKEN
 
 if __name__ == '__main__':
     app.run(debug=True)
