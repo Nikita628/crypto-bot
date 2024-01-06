ccbeb5e7e5d3f9b0b38c8a46f14553f6da83f240
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-06 15:42:32 +0300

    add flask server

--- www/deployment_server/app.py
+++ www/deployment_server/app.py
@@ -1,41 +1,18 @@
 from flask import Flask, request
-import os
-from dotenv import load_dotenv
-from subprocess import call
-from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def do_post():
     if request.method == 'POST':
-        if(request.args.get('ref')):
-            with open('output.txt', 'a') as f:
-                f.write(request.args.get('ref'))
+        if request.args.get('ref') == 'refs/heads/migrations_and_python_server':
+            with open('./output.txt', 'a') as f:
+                f.write('Hi')
+            return 'r'
         else:
-            with open('output.txt', 'a') as f:
-                f.write('end')
-
-        request.args.get('ref') == 'refs/heads/migrations_and_python_server':
-            try:
-                call("/var/www/scripts/deploying 2> /var/www/logs/deploying_err.log", shell=True)
-
-                load_dotenv()
-                CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
-                CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
-                SEND_URL = f'https://api.telegram.org/bot{CRYPTO_BOT_TOKEN}/sendMessage'
-                message = f'<b>Crypto-bot message</b>\n'
-                message += f'<b>Action:</b> update files\n'
-                message += f'<b>Result:</b> success\n'
-                message += f'<b>DateTime:</b> ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-                response = requests.post(SEND_URL, json={'chat_id': CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
-                if response:
-                    print('result is sent to telegram')
-                else:
-                    print('error sending to telegram')
-
-            except:
-                print('update error')
+            with open('./output.txt', 'a') as f:
+                f.write('TTTTT')
+            return 'post'
     else:
         return 'not post request'
 
