7aa03ebddbec32adbd5a066febbc8bccb3b38615
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-10 07:39:50 +0300

    update deployment

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from subprocess import call
 from datetime import datetime
+import time
 import requests
 import json
 
@@ -10,17 +11,18 @@ app = Flask(__name__)
 
 load_dotenv()
 _CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
+_CRYPTO_BOT_SIGNALS_CHAT_ID = os.getenv('CRYPTO_BOT_SIGNALS_CHAT_ID')
 _CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
 SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
 
 @app.route('/', methods=['POST'])
 def do_post():
     git_event = json.loads(request.data)
-    if git_event.get('ref') == 'refs/heads/prod':
+    if git_event.get('ref') == 'refs/heads/migrations_and_python_server':
         update_result = 'success'
 
         try:
-            call("/var/www/scripts/deployment 2> /var/www/logs/deployment_err.log", shell=True)
+            call('/var/www/scripts/deployment 2> /var/www/logs/deployment_err.log', shell=True)
         except:
             update_result = 'error'
 
@@ -28,12 +30,13 @@ def do_post():
         <b>Action:</b> update files
         <b>Result:</b> {update_result}
         <b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
-        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
 
         # retry if failed
         if not response:
             count = 1
             while (not response and count <= 5):
+                time.sleep(5)
                 response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
                 count += 1
 
