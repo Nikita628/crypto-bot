bb5d67cd48db6c5e5b42969de5e2f1c5aef8e279
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-10 09:52:05 +0300

    update deployment

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -26,11 +26,7 @@ def do_post():
         except:
             update_result = 'error'
 
-        message = f'''
-        <b>Crypto-bot message</b>
-        <b>Action:</b> update files
-        <b>Result:</b> {update_result}
-        <b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
+        message = f'''<b>Crypto-bot message</b><b>Action:</b> update files<b>Result:</b> {update_result}<b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
         response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
 
         # retry if failed
