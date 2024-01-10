0c796c4105ec1f5681795515bb3039958c55107d
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-10 13:34:46 +0300

    test

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -12,7 +12,6 @@ app = Flask(__name__)
 
 load_dotenv()
 _CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
-_CRYPTO_BOT_SIGNALS_CHAT_ID = os.getenv('CRYPTO_BOT_SIGNALS_CHAT_ID')
 _CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
 SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
 
@@ -20,7 +19,7 @@ SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
 @app.route('/', methods=['POST'])
 def do_post():
     git_event = json.loads(request.data)
-    if git_event.get('ref') == 'refs/heads/migrations_and_python_server':
+    if git_event.get('ref') == 'refs/heads/prod':
         update_result = 'success'
 
         try:
@@ -33,14 +32,14 @@ def do_post():
 <b>Action:</b> update files
 <b>Result:</b> {update_result}
 <b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
-        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
 
         # retry if failed
         if not response:
             count = 1
             while (not response and count <= 5):
                 time.sleep(5)
-                response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+                response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
                 count += 1
 
         return 'prod branch action'
