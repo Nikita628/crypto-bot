4143959b51f90e7531bbd1d445b233dd064e640e
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-11 14:55:12 +0300

    test

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -10,6 +10,7 @@ import json
 app = Flask(__name__)
 
 load_dotenv()
+_CRYPTO_BOT_SIGNALS_CHAT_ID = os.getenv('CRYPTO_BOT_SIGNALS_CHAT_ID')
 _CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
 _CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
 SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
@@ -30,14 +31,14 @@ def do_post():
 <b>Action:</b> update files
 <b>Result:</b> {update_result}
 <b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
-        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
 
         # retry if failed
         if not response:
             count = 1
             while (not response and count <= 5):
                 time.sleep(5)
-                response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+                response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_SIGNALS_CHAT_ID, 'parse_mode': 'html', 'text': message})
                 count += 1
 
         return 'prod branch action'
