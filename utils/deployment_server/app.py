fe7d084932126355225ffc278cf28de1b4164c1b
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-15 20:20:15 +0300

    auto deploying (#6)
    * env config
    
    * test
    
    * test
    
    * test
    
    * test
    
    * update deploying
    
    * test
    
    * comment NEED_TO_POST_SIGNALS_IN_TG (trigger error)
    
    * test
    
    * test
    
    * test
    
    * change yml
    
    * dep
    
    * test
    
    * test
    
    * add restart server
    
    * comment tg constant
    
    * add flask server
    
    * add flask server
    
    * add flask server
    
    * add flask server
    
    * add flask server
    
    * add flask server
    
    * remove
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * add
    
    * update files
    
    * test
    
    * test
    
    * dep test
    
    * dep test
    
    * dep test
    
    * pull request edits
    
    * resolving conflict
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * update deployment
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * ssssss
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test

---
+++ utils/deployment_server/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request
+import os
+from dotenv import load_dotenv
+from subprocess import call
+from datetime import datetime
+import time
+import requests
+import json
+
+app = Flask(__name__)
+
+load_dotenv()
+_CRYPTO_BOT_SIGNALS_CHAT_ID = os.getenv('CRYPTO_BOT_SIGNALS_CHAT_ID')
+_CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
+_CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
+SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
+
+@app.route('/', methods=['POST'])
+def do_post():
+    git_event = json.loads(request.data)
+    if git_event.get('ref') == 'refs/heads/prod':
+        result_message = 'success'
+
+        try:
+            result = call('/var/bot-app/crypto-bot/utils/scripts/git_pull', shell=True)
+            if result != 0:
+                result_message = 'git pull error'
+            else:
+                result = call('/var/bot-app/crypto-bot/utils/scripts/docker_compose_and_migrations', shell=True)
+                if result != 0:
+                    result_message = 'compose or migrations error'
+
+        except:
+            result_message = 'error'
+
+        error_label = ''
+        if result_message != 'success':
+            error_label = '<b>ERROR</b> '
+
+        message = f'''
+<b>{error_label}Crypto-bot message</b>
+<b>Action:</b> auto deployment
+<b>Result:</b> {result_message}
+<b>DateTime:</b> {datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}'''
+        response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+
+        # retry if failed
+        if not response:
+            count = 1
+            while (not response and count <= 5):
+                time.sleep(5)
+                response = requests.post(SEND_URL, json={'chat_id': _CRYPTO_BOT_STATUS_CHAT_ID, 'parse_mode': 'html', 'text': message})
+                count += 1
+
+        return 'prod branch action'
+    else:
+        return 'other branch action'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
