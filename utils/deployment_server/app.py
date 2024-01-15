a2f501ecb565028529642f04e1ae674d33af6889
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-01-15 16:39:53 +0300

    test

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -30,7 +30,7 @@ def do_post():
             else:
                 result = call('/var/bot-app/crypto-bot/utils/scripts/docker_compose_and_migrations', shell=True)
                 with open('/root/lll', 'a') as f:
-                    f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + str(result))
+                    f.write(datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + '  ---  ' + str(result))
                     f.write('exit')
                 if result != 0:
                     result_message = 'compose or migrations error'
