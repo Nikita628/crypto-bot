5cad2d104eec9a3792b5400aa783e7af471626ea
junkysoul <116711840+junkysoul@users.noreply.github.com>
2024-02-05 17:58:42 +0300

    upd

--- utils/deployment_server/app.py
+++ utils/deployment_server/app.py
@@ -15,8 +15,9 @@ _CRYPTO_BOT_STATUS_CHAT_ID = os.getenv('CRYPTO_BOT_STATUS_CHAT_ID')
 _CRYPTO_BOT_TOKEN = os.getenv('CRYPTO_BOT_TOKEN')
 SEND_URL = f'https://api.telegram.org/bot{_CRYPTO_BOT_TOKEN}/sendMessage'
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def do_post():
+    if request.method == 'POST':
     git_event = json.loads(request.data)
     if git_event.get('ref') == 'refs/heads/prod':
         result_message = 'success'
