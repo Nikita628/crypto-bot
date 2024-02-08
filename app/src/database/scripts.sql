d26c98664506599105c8949c54a025dcfe388234
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2024-02-08 14:19:13 +0400

    stats script

--- app/src/database/scripts.sql
+++ app/src/database/scripts.sql
@@ -8,3 +8,24 @@ group by strategy;
 select * from trade
 where strategy = 'dual_momentum'
 order by entry_date
+
+
+
+SELECT 
+strategy, 
+
+count(*) as total_trades,
+sum(profit_percentage) as "total_profit%",
+
+(select count(*) from trade where exit_date is null and strategy = tr.strategy) as opened_trades,
+(select sum(profit_percentage) from trade where exit_date is null and strategy = tr.strategy) as "opened_trades_profit%",
+
+(select count(*) from trade where exit_date is not null and strategy = tr.strategy) as closed_trades,
+(select sum(profit_percentage) from trade where exit_date is not null and strategy = tr.strategy) as "closed_trades_profit%",
+
+(select count(*) from trade where exit_date is not null and strategy = tr.strategy and profit_percentage > 0) as closed_in_profit_trades,
+(select count(*) from trade where exit_date is not null and strategy = tr.strategy and profit_percentage <= 0) as closed_in_loss_trades
+
+from trade tr
+group by strategy
+order by "total_profit%" desc;
