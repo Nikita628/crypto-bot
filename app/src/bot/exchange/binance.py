53423cbfd3f4f50b26a7fef9c573a96fca85874b
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2024-02-09 14:50:20 +0400

    fixed symbols error

--- app/src/bot/exchange/binance.py
+++ app/src/bot/exchange/binance.py
@@ -105,9 +105,8 @@ def cache_usdt_symbols_list():
             _USDT_SYMBOLS = json.load(file)
     else:
         _USDT_SYMBOLS = _filter_out_dangerous_symbols(get_all_usdt_symbols())
-
-    with open(file_path, 'w') as file:
-        json.dump(_USDT_SYMBOLS, file)
+        with open(file_path, 'w') as file:
+            json.dump(_USDT_SYMBOLS, file)
 
 
 def _filter_out_dangerous_symbols(all_usdt_symbols: List[str]) -> List[str]:
